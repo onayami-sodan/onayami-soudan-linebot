d8a218881c87347c7f5d2b8a940297d8b4233b5f
onayami-sodan <webmaster@onayami-sodan.com>
2025-05-23 16:52:15 +0900

    Update supabaseClient.js

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,8 +1,8 @@
 // supabaseClient.js
-import { createClient } from '@supabase/supabase-js'
+import 'dotenv/config'; // ← これを最初に追加！
+import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.SUPABASE_URL
-const supabaseKey = process.env.SUPABASE_ANON_KEY
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_ANON_KEY;
 
-export const supabase = createClient(supabaseUrl, supabaseKey)
-
+export const supabase = createClient(supabaseUrl, supabaseKey);
