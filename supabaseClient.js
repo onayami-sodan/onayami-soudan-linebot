c94fc517a715f8040076378cd0d3618d7ec4692b
onayami-sodan <webmaster@onayami-sodan.com>
2025-09-08 17:06:15 +0900

    Update supabaseClient.js

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,10 +1,6 @@
-// supabaseClient.js
-const { createClient } = require('@supabase/supabase-js')
+import { createClient } from '@supabase/supabase-js'
 
-const supabase = createClient(
-  process.env.SUPABASE_URL,
-  process.env.SUPABASE_ANON_KEY,
-  { auth: { persistSession: false } }
-)
+const supabaseUrl = process.env.SUPABASE_URL
+const supabaseKey = process.env.SUPABASE_ANON_KEY
 
-module.exports = { supabase }
+export const supabase = createClient(supabaseUrl, supabaseKey)
